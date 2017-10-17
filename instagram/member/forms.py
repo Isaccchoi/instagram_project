e1539e9cb1ac4d0a41aa6fae7f92df99ab33d152
isaccchoi <isaccchoi@naver.com>
2017-10-17 16:05:15 +0900

    SignupForm 및 UserForm에 clean 재정의,custom 메서드 생성

--- instagram/member/forms.py
+++ instagram/member/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate, login
 
 User = get_user_model()
 
@@ -21,9 +21,74 @@ class UserForm(forms.Form):
             }
         ),
     )
+    password2 = forms.CharField(
+        required=False,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control'
+            }
+        )
+    )
 
     def clean_username(self):
         data = self.cleaned_data['username']
         if User.objects.filter(username=data).exists():
             raise forms.ValidationError('이미 존재하는 계정입니다!')
         return data
+
+    def clean_password2(self):
+        password = self.cleaned_data['password']
+        password2 = self.cleaned_data['password2']
+        if not password == password2:
+            raise forms.ValidationError('비밀번호가 같지 않습니다.')
+        else:
+            setattr(self, 'signup', self._signup)
+        return password2
+
+    def _signup(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = User.objects.create_user(username=username, password=password)
+        if user is not None:
+            login(request, user)
+
+
+class LoginForm(forms.Form):
+    """
+    is_valid()에서 주어진 username/password를 사용한 authenticate실행 성공시 login(request)메서드를 사용할 수 있음
+    """
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control'
+            }
+        )
+    )
+    password = forms.CharField(
+        required=False,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control'
+            }
+        )
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user = None
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+        self.user = authenticate(username=username, password=password)
+        if not self.user:
+            raise forms.ValidationError("계정 이름 또는 암호가 맞지 않습니다.")
+        else:
+            # clean에서 user를 넣었을때만 self._login을 self.login으로 사용할 수 있도록 지
+            setattr(self, 'login', self._login)
+        return cleaned_data
+
+    def _login(self, request):
+        if self.user is not None:
+            login(request, self.user)
--- instagram/member/views.py
+++ instagram/member/views.py
@@ -1,8 +1,7 @@
-from django.contrib.auth import get_user_model, authenticate, login as django_login, logout as django_logout
-from django.http import HttpResponse
+from django.contrib.auth import get_user_model, logout as django_logout
 from django.shortcuts import render, redirect
 
-from .forms import UserForm
+from .forms import UserForm, LoginForm
 
 User = get_user_model()
 
@@ -11,32 +10,28 @@ def signup(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            User.objects.create_user(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
-        context = {
-            'user_form': UserForm
-        }
+            form.signup(request)
+            return redirect('post:post_list')
     else:
-        context = {
-            'user_form': UserForm,
-        }
+        form = UserForm
+    context = {
+        'user_form': form,
+    }
     return render(request, 'member/signup.html', context)
 
 
 def login(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(
-            username=username,
-            password=password
-        )
-        if user is not None:
-            django_login(request, user)
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            form.login(request)
             return redirect('post:post_list')
-        else:
-            return HttpResponse('Login credentials invalid')
     else:
-        return render(request, 'member/login.html')
+        form = LoginForm
+    context = {
+        'form': form
+    }
+    return render(request, 'member/login.html', context)
 
 
 def logout(request):
